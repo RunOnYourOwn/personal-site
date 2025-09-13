78ae933cdee7d9e3b9cd209ea327b461c5a97afa
Aaron Brazier <aaronbrazier@gmail.com>
2025-09-13 09:07:49 -0500

    updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Aaron Brazier';
+export const SITE_DESCRIPTION =
+  'Senior Data Scientist specializing in machine learning, oil & gas analytics, and homelab infrastructure. Portfolio, blog, and project showcase.';
